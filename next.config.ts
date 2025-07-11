51dc55e4133f8993d39e607c1e60be51761add25
Harvar3105 <Petrochenko03@inbox.ru>
2025-07-11 15:16:58 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
-import path from 'path';
 
 const isProd = process.env.NODE_ENV ?? 'production';
-const withNextIntl = createNextIntlPlugin(path.resolve(__dirname, 'src/i18n/request.ts'));
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
