33f4ab1bfc6e9158638f7b16d28cbc2b071601a1
Harvar3105 <Petrochenko03@inbox.ru>
2025-07-12 01:24:32 +0300

    deploy fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,3 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import Footer from "@/components/layouts/Footer";
 import Header from "@/components/layouts/Header";
@@ -7,16 +6,6 @@ import {NextIntlClientProvider, hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
 import {getTranslations } from "next-intl/server";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
@@ -53,16 +42,10 @@ export default async function LocaleLayout({
   }
  
   return (
-    <html lang={locale}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
-      >
-        <NextIntlClientProvider locale={locale}>
-          <Header />
-            <main className="flex-grow">{children}</main>
-          <Footer />
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextIntlClientProvider locale={locale}>
+      <Header />
+        <main className="flex-grow">{children}</main>
+      <Footer />
+    </NextIntlClientProvider>
   );
 }
