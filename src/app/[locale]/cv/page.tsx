3ed339f155422e638b9e54f4895770dc410fd4ac
Harvar3105 <Petrochenko03@inbox.ru>
2025-07-21 10:12:01 +0300

    added Ru CV

--- src/app/[locale]/cv/page.tsx
+++ src/app/[locale]/cv/page.tsx
@@ -2,7 +2,7 @@ import Fireflies from "@/components/animations/fireflies";
 import FileDownloader from "@/components/widgets/FileDownloader";
 import PdfViewer from "@/components/widgets/PdfViewer";
 import { useTranslations } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
+import { getLocale, setRequestLocale } from "next-intl/server";
 import { use } from "react";
 
 export default function CV({params}: {params: Promise<{locale: string}>;}){
@@ -13,9 +13,8 @@ export default function CV({params}: {params: Promise<{locale: string}>;}){
 
     return(
         <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            {/* TODO: if RU, show RU CV. Create RU CV */}
-            <PdfViewer url="/pdf/CV.pdf" />
-            <FileDownloader url="/pdf/CV.pdf" filename="Juri_Petrotsenko_CV" label={t("download")} />
+            <PdfViewer url={locale === "en" ? "/pdf/CV.pdf" : "/pdf/CV_Rus.pdf"} />
+            <FileDownloader url={locale === "en" ? "/pdf/CV.pdf" : "/pdf/CV_Rus.pdf"} filename="Juri_Petrotsenko_CV" label={t("download")} />
             <Fireflies count={50} />
         </div>
     );
