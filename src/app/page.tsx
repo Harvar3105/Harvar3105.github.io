5f71f95abbbee11e7c05d8df69043d55052bff8a
Harvar3105 <Petrochenko03@inbox.ru>
2025-07-26 15:50:27 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-import { redirect } from 'next/navigation';
+"use client";
+
+import { redirect, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function RootRedirect() {
-  redirect('/en');
+  // redirect('/en');
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/en");
+  }, [router]);
+
+  return (
+    <div>
+      <p>Redirecting to English version...</p>
+    </div>
+  );
 }
